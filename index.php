57114f13d945edacc07cf044ba270e8300102950
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-11-06 13:31:17 +0100

    feat: temp page

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
     <link rel="icon" href="" type="image/x-icon" />
     
     <meta name="application-name" content="PulsAid">
-    <meta name="description" content="">
-    <meta name="keywords" content="some keywords">
+    <meta name="description" content="Providing faster assistance to people with heart failure, therefor increasing chances of survival.">
+    <meta name="keywords" content="Heart, Assistance, Health, Resucitacion">
     <meta name="author" content="Lindsay Peeters, Evelien Vanophalvens, Yorick Devleeschouwer">
     <!-- open graphs -->
     <meta property="og:type" content="website" />
     <meta property="og:secure_url" content="https://www.pulsaid.be" />
     <meta property="og:url" content="http://www.pulsaid.be" />
     <meta property="og:title" content="Pulsaid | someslogan" />
-    <meta property="og:description" content="" />
+    <meta property="og:description" content="Providing faster assistance to people with heart failure, therefor increasing chances of survival." />
     <meta property="og:image" content="" />
     <meta property="og:site_name" content="PulsAid" />
 
@@ -23,12 +23,25 @@
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:site" content="pulsaid.be" />
     <meta property="twitter:title" content="Pulsaid | someslogan" />
-    <meta property="twitter:description" content="" />
+    <meta property="twitter:description" content="Providing faster assistance to people with heart failure, therefor increasing chances of survival." />
     <meta property="twitter:image" content="" />
 
     <title>PulsAid - Coming Soon</title>
 </head>
 <body>
-    <h1>Coming soon...</h1>
+    <h1>PulsAid</h1>
+    <div>
+        <p>Coming soon</p>
+    </div>
+    <footer class="footer">
+    <p>Follow us on our socials:</p>
+    <div>
+        <a href="https://www.facebook.com/profile.php?id=61553209742298" target="_blank"><img src="https://portfolionew.blob.core.windows.net/pulsaid/icons/facebook.png?sp=r&st=2023-11-06T12:27:29Z&se=2030-03-07T20:27:29Z&spr=https&sv=2022-11-02&sr=c&sig=8Rm7dlc%2F%2BmJsqeFwO8JzpZAZ8W3U%2B6NKiyt8gR1Jr20%3D" alt="facebook" style="width: 30px; margin: 0 10px;"></a>
+        <a href="https://www.instagram.com/pulsaid.be/" target="_blank"><img src="https://portfolionew.blob.core.windows.net/pulsaid/icons/instagram.png?sp=r&st=2023-11-06T12:27:29Z&se=2030-03-07T20:27:29Z&spr=https&sv=2022-11-02&sr=c&sig=8Rm7dlc%2F%2BmJsqeFwO8JzpZAZ8W3U%2B6NKiyt8gR1Jr20%3D" alt="instagram" style="width: 30px; margin: 0 10px;"></a>
+        <a href="https://www.tiktok.com/@pulsaid.be" target="_blank"><img src="https://portfolionew.blob.core.windows.net/pulsaid/icons/tiktok.png?sp=r&st=2023-11-06T12:27:29Z&se=2030-03-07T20:27:29Z&spr=https&sv=2022-11-02&sr=c&sig=8Rm7dlc%2F%2BmJsqeFwO8JzpZAZ8W3U%2B6NKiyt8gR1Jr20%3D" alt="tiktok" style="width: 30px; margin: 0 10px;"></a>
+        <a href="https://www.youtube.com/channel/UCGXcRqts6DdVYfmwh3V3IwQ" target="_blank"><img src="https://portfolionew.blob.core.windows.net/pulsaid/icons/youtube.png?sp=r&st=2023-11-06T12:27:29Z&se=2030-03-07T20:27:29Z&spr=https&sv=2022-11-02&sr=c&sig=8Rm7dlc%2F%2BmJsqeFwO8JzpZAZ8W3U%2B6NKiyt8gR1Jr20%3D" alt="youtube" style="width: 30px; margin: 0 10px;"></a>
+        
+    </div>
+</footer>
 </body>
 </html>
